8182b1bc0ed8971edc724d43201db904bdcd20fb
systemaworking <69110904+systemaworking@users.noreply.github.com>
2022-07-08 13:04:42 +0300

    +

--- stop-all.sh
+++ stop-all.sh
@@ -4,6 +4,6 @@ echo "Close all python marked by 'grabber-bbb-mustansir'"
 ps aux | grep "grabber-bbb-mustansir" | grep "python" | awk '{print $2}' | xargs -r kill -9
 
 echo "Close all chromedriver marked by 'grabber-bbb-mustansir'"
-ps aux | grep "grabber-bbb-mustansir" | grep "chrome" | awk '{print $3}' | xargs -r kill -9
+ps -ef | grep "grabber-bbb-mustansir" | grep "chrome" | awk '{print $3}' | xargs -r kill -9
 
 echo "Done"
