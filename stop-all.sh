ba0f9a4eec0044d1c720be99a2509f1727f39545
systemaworking <69110904+systemaworking@users.noreply.github.com>
2022-07-08 12:52:12 +0300

    fix

--- stop-all.sh
+++ stop-all.sh
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 #!/bin/bash
 
 echo "Close all processes marked by 'grabber-bbb-mustansir'"
 ps aux | grep "grabber-bbb-mustansir" | awk '{print $2}' | xargs -r kill -9
 
 echo "Done"
->>>>>>> f3bcc7caf9f287968abfcaecf573e440a86c1723
