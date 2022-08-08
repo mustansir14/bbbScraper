5df5a8b4b39158b435a827b46843e1a99a6ff5d0
Mustansir Muzaffar <mustansir2001@gmail.com>
2022-08-08 17:55:50 +0500

    google chrome added

--- install/install.sh
+++ install/install.sh
@@ -2,4 +2,5 @@ yum install python3 -y
 yum install python3-pip -y
 yum install MariaDB-shared MariaDB-devel -y
 yum install Xvfb -y
+yum install google-chrome-stable -y
 pip install -r install/requirements.txt
